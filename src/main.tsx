500bfdae0db3f04a38548d93886d98c799ceb157
ej-shafran <ejshafran@hilma.tech>
2023-12-06 10:55:27 +0200

    Format

--- src/main.tsx
+++ src/main.tsx
@@ -7,10 +7,10 @@ import { wrap } from "@hilma/tools";
 import { AlertProvider } from "@hilma/forms";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-import App from "./App";
-import { I18nProvider } from "./common/i18n";
 import { createTheme, ThemeProvider } from "@mui/material";
+
+import { I18nProvider } from "./common/i18n";
+import App from "./App";
 
 const client = new QueryClient();
 
